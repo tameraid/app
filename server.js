0389bf2a9036bd5c52b2d5ff396ca9e6d734b1c4
tameraid <tameraid@yahoo.com>
2020-02-02 01:48:59 +0200

    add

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const express = require('express')
 
 const app = require('./src/app')
 
-// app.use( express.static('client/build') )
-
-// app.get('*' , (req,res)=>{
-//     res.sendFile(path.resolve(__dirname , 'client' , 'index.html' ))
-// } )
 
 const PORT = process.env.PORT || 5000
 

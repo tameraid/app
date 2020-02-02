106457ea27db455d14be49c3a5df4dd215353383
330tamer848 <tameraid@yahoo.com>
2020-02-03 00:23:48 +0200

    Updates

---
+++ precache-manifest.1329411902c13358137103742bd3a6a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4858fee82cb3bf605471c5fcd53328a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "504c133d9ea6a7508d67",
+    "url": "/static/css/main.59fdaa60.chunk.css"
+  },
+  {
+    "revision": "9074282fa0cc7aec7e90",
+    "url": "/static/js/2.9058f80e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9058f80e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "504c133d9ea6a7508d67",
+    "url": "/static/js/main.5ec7f174.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
